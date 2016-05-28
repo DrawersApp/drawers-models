ba4a3b388add009e93dbe6131d23e6b17fadc2d4
Harshit <harshit.bangar@gmail.com>
2016-05-28 13:30:43 +0530

    code for pending messages added

---
+++ src/main/java/com/drawers/dao/PendingSubscribeStore.java
@@ -0,0 +1,10 @@
+package com.drawers.dao;
+
+import java.util.Set;
+
+/**
+ * Created by harshit on 28/5/16.
+ */
+public interface PendingSubscribeStore {
+    Set<String> getPendingSubscribers();
+}
